f4a12487559f0e9a9f4e5d30b9c55fbb54dc6db3
Ian Holm <ianholm252@gmail.com>
2021-08-31 12:04:51 -0600

    added entry log

--- monitoring/server.js
+++ monitoring/server.js
@@ -6,12 +6,23 @@ let rollbar = new Rollbar({
     captureUncaught: true,
     captureUnhandledRejections: true
 })
-
+const students = []
 const app = express()
+
+app.use(rollbar.errorHandler())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html files served')
+})
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+    students.push(name)
+    rollbar.log("student added", {author: "Ian", type: "manual entry"})
+
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4545
