9fcefb8f6289c23f7c68a2685e57180e4d077d80
Ian Holm <ianholm252@gmail.com>
2021-08-31 11:16:02 -0600

    added files

--- monitoring/server.js
+++ monitoring/server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.listen(4545, () => {
+const port = process.env.PORT || 4545
+
+app.listen(port, () => {
     console.log('running on 4545')
 })
